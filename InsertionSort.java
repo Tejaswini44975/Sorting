0452651cbe43460b40d59daa2b5bd1390e5f8d40
Tejaswini SV <142816763+Tejaswini44975@users.noreply.github.com>
2023-09-11 18:47:31 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,22 @@
+class Solution
+{
+  static void insert(int arr[],int i)
+  {
+       int key=arr[i];
+       int j=i-1;
+       while(j>=0 && arr[j]>key)
+       {
+           arr[j+1]=arr[j];
+           j=j-1;
+       }
+         arr[j+1]=key;
+  }
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  {
+     for(int i=0;i<n;i++)
+     {
+         insert(arr,i);
+     }
+  }
+}
